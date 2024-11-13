8153345a3ea8deebefb54f539e5757cb9c520f5f
bled21454 <bled21454@gmail.com>
2024-11-13 21:34:08 +0900

    Create 옹알이(1).py

---
+++ coding_test/옹알이(1).py
@@ -0,0 +1,17 @@
+# 프로그래머스 - 옹알이(1)
+# https://school.programmers.co.kr/learn/courses/30/lessons/120956
+def solution(babbling):
+    answer = 0
+    a = ["aya", "ye", "woo", "ma" ]
+    for i in range(1,5):
+        for j in permutations(a, i):
+            tmp = ''
+            for k in j:
+                tmp += k
+            all.append(tmp)
+
+    for b in babbling:
+        if b in all:
+            answer += 1
+    
+    return answer
